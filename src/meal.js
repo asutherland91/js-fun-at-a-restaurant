de6ee4c08ade1f65774761eb48ed67227d70bc61
Adriane Sutherland <asutherland91@gmail.com>
2023-02-01 19:21:18 -0800

    add meal.js and warm-up.js soulutions

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
 }
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+  return menuItem;
+}
+
+function addIngredients(itemToAdd, ingredients) {
+  if(!ingredients.includes(itemToAdd)) {
+    ingredients.push(itemToAdd);
+  }
+}
+function formatPrice(price) {
+  var formattedPrice = `$${price}`;
+return formattedPrice;
+}
+
+function decreasePrice(price) {
+  var decreasedPrice = (price -(price * 0.1));
+return decreasedPrice;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+title: title,
+ingredients: ingredients,
+type: menuItemType
+  }
+  return recipe
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
